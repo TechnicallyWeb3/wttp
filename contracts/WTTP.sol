0bb173330f393b7be3b0ad0753cb5854f1579251
Technically Web3 <mancinoweb3@gmail.com>
2024-11-06 20:33:14 -0700

    added ifModifiedSince to the 304 return

--- contracts/WTTP.sol
+++ contracts/WTTP.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import "./WebStorage.sol";
 import "./WebContract.sol";
 
-
 struct RequestHeader {
     bytes2[] accept;
     bytes2[] acceptCharset;
@@ -25,10 +24,12 @@ struct GETResponse {
 }
 
 contract WTTP {
-
-    function _methodAllowed(uint16 _methods, Method _method) internal pure returns (bool) {
+    function _methodAllowed(
+        uint16 _methods,
+        Method _method
+    ) internal pure returns (bool) {
         uint16 methodBit = uint16(1 << uint8(_method)); // Create a bitmask for the method
-        
+
         if (_methods & methodBit != 0) {
             return true;
         }
@@ -36,39 +37,66 @@ contract WTTP {
         return false;
     }
 
-    function GET(RequestLine memory _requestLine, RequestHeader memory _requestHeader, GETRequest memory _getRequest) public view returns (GETResponse memory getResponse) {
-        LOCATEResponse memory _locateResponse = WTTPBaseMethods(_getRequest.host).LOCATE(_requestLine);
-        DataPointStorage _dataPointStorage = WTTPBaseMethods(_getRequest.host).DPR_().DPS_();
+    function GET(
+        RequestLine memory _requestLine,
+        RequestHeader memory _requestHeader,
+        GETRequest memory _getRequest
+    ) public view returns (GETResponse memory getResponse) {
+        LOCATEResponse memory _locateResponse = WTTPBaseMethods(
+            _getRequest.host
+        ).LOCATE(_requestLine);
+        DataPointStorage _dataPointStorage = WTTPBaseMethods(_getRequest.host)
+            .DPR_()
+            .DPS_();
         getResponse.head = _locateResponse.head;
 
         // if (!_methodAllowed(getResponse.head.headerInfo.methods, Method.GET)) {
         //     getResponse.responseLine = ResponseLine(_requestLine.protocol, 406);
         //     return getResponse;
-        // } else 
-        
-        if (getResponse.head.etag != bytes32(0) && getResponse.head.etag == _requestHeader.ifNoneMatch) {
-            getResponse.head.responseLine = ResponseLine(_requestLine.protocol, 304);
+        // } else
+
+        if (
+            (getResponse.head.etag != bytes32(0) &&
+                getResponse.head.etag == _requestHeader.ifNoneMatch) ||
+            (getResponse.head.metadata.modifiedDate != 0 &&
+                _requestHeader.ifModifiedSince >= getResponse.head.metadata.modifiedDate)
+        ) {
+            getResponse.head.responseLine = ResponseLine(
+                _requestLine.protocol,
+                304
+            );
             return getResponse;
         }
-        
+
         if (getResponse.head.responseLine.code == 200) {
             bytes32[] memory _dataPoints = _locateResponse.dataPoints;
             uint32 start = _getRequest.rangeStart;
             uint32 end = _getRequest.rangeEnd;
             if (end >= _dataPoints.length || start > end) {
-                getResponse.head.responseLine = ResponseLine(_requestLine.protocol, 416);
+                getResponse.head.responseLine = ResponseLine(
+                    _requestLine.protocol,
+                    416
+                );
                 return getResponse;
             } else if (end == 0) {
                 end = uint32(_dataPoints.length) - 1;
             }
 
             if (start > 0 || end < _dataPoints.length - 1) {
-                getResponse.head.responseLine = ResponseLine(_requestLine.protocol, 206);
+                getResponse.head.responseLine = ResponseLine(
+                    _requestLine.protocol,
+                    206
+                );
             }
 
-            getResponse.body = _dataPointStorage.readDataPoint(_dataPoints[start]).data;
+            getResponse.body = _dataPointStorage
+                .readDataPoint(_dataPoints[start])
+                .data;
             for (uint256 i = start + 1; i <= end; i++) {
-                getResponse.body = abi.encodePacked(getResponse.body, _dataPointStorage.readDataPoint(_dataPoints[i]).data);
+                getResponse.body = abi.encodePacked(
+                    getResponse.body,
+                    _dataPointStorage.readDataPoint(_dataPoints[i]).data
+                );
             }
         }
     }
